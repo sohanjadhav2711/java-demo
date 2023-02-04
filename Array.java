898c333f13f7d48daa0630d7c254f303ca18caf6
sohanjadhav2711 <sohanjadhav2711@gmail.com>
2023-02-04 05:48:22 +0530

    first commit

---
+++ Array.java
@@ -0,0 +1,16 @@
+imort java.util.*;
+ public class Array {
+     public static void main(String[] args) {
+        int marks []= new int [3];
+        marks[0]=97;
+        marks[1]=67;
+        marks[2]=87;
+        system.out.println(marks[0]);
+
+
+
+        
+    }
+        
+    }
+ 
